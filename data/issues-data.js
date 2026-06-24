3947e84069ba931a2a6645f96c34f3421b9b271e
nishantpatnaik10-maker <nishantpatnaik10@gmail.com>
2026-06-24 21:16:39 +1000

    Update issues [24/06/2026, 21:16:39]

--- data/issues-data.js
+++ data/issues-data.js
@@ -1,5 +1,5 @@
 // ISSUES / KANBAN DATA FILE
-// Auto-updated by Sync to GitHub on 2026-06-24T11:14:12.326Z
+// Auto-updated by Sync to GitHub on 2026-06-24T11:16:39.610Z
 // Do not edit manually — use the dashboard.
 
 const INITIAL_ISSUES = [
@@ -13,5 +13,27 @@ const INITIAL_ISSUES = [
     "gameboard": "",
     "id": "issue-1782299604677",
     "createdAt": "2026-06-24T11:13:24.677Z"
+  },
+  {
+    "title": "Org field - Free text",
+    "description": "Org field on the game boards to be changed from a lookup to a free text",
+    "priority": "Medium",
+    "category": "UI/UX",
+    "status": "backlog",
+    "assignee": "Wojciech",
+    "gameboard": "",
+    "id": "issue-1782299735510",
+    "createdAt": "2026-06-24T11:15:35.510Z"
+  },
+  {
+    "title": "Upgrade to 26.1 GA",
+    "description": "Upgrade all environments to 26.1 GA",
+    "priority": "Medium",
+    "category": "Other",
+    "status": "backlog",
+    "assignee": "Nishant",
+    "gameboard": "",
+    "id": "issue-1782299792044",
+    "createdAt": "2026-06-24T11:16:32.044Z"
   }
 ];
